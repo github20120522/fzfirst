e4901a05e0faec281b3dfbb125e856e66e3f9da7
super9192010@gmail.com <super9192010@gmail.com@5088c0b7-6ed0-9e50-4eab-d3741cb22736>
2010-11-17 08:21:31 +0000

    ibatis学习 增删查方法，同时试验一下svn提交能不能提交到Google上面

---
+++ trunk/ibatisTest/src/com/fzibatis/Student.java
@@ -0,0 +1,36 @@
+package com.fzibatis;
+
+import java.sql.Date;
+
+public class Student {
+	private int id = 0;
+	private String sname = null;
+	private Date birth = null;
+	private int age = 0;
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+}
